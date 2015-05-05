e707a7dfec4daff423cdf1035ea93df2843caf3a
Jason Ody <jasonody@users.noreply.github.com>
2015-05-04 22:46:13 -0700

    Extract nav for nav directive and added login fields

---
+++ NamaBeer.Client/app/ui/nav/NavController.js
@@ -0,0 +1,21 @@
+﻿(function () {
+	'use strict';
+
+	angular
+		.module('nama.ui')
+		.controller('NavController', navController);
+
+	navController.$inject = [];
+
+	function navController() {
+
+		var vm = this;
+
+		vm.isCollapsed = true;
+		vm.toggleCollapsed = function () {
+
+			vm.isCollapsed = !vm.isCollapsed;
+		};
+	};
+
+}());
---
+++ NamaBeer.Client/app/ui/nav/navDirective.js
@@ -0,0 +1,22 @@
+﻿(function () {
+	'use strict';
+
+	angular
+		.module('nama.ui')
+		.directive('namaNav', namaNav);
+
+	namaNav.$inject = [];
+
+	function namaNav() {
+
+		return {
+			restrict: 'E',
+			templateUrl: '/app/ui/nav/nav.html',
+			controller: 'NavController',
+			controllerAs: 'vm',
+			bindController: true,
+			scope: {}
+		};
+	}
+
+}());
