f6150c268528ecf88450e80563a62de6173de0ec
Jason Ody <jasonody@users.noreply.github.com>
2015-05-06 22:38:23 -0700

    Extracted form validation messages to a directive

---
+++ NamaBeer.Client/app/ui/forms/formerrorsDirective.js
@@ -0,0 +1,38 @@
+﻿(function () {
+	'use strict';
+
+	angular
+		.module('nama.ui')
+		.directive('namaFormerrors', namaFormerrors);
+
+	namaFormerrors.$inject = ['$compile'];
+
+	function namaFormerrors($compile) {
+
+		return {
+			restict: 'AE',
+			require: '^form',
+			link: function (scope, element, attributes, controller) {
+				
+				var inputName = attributes.for;
+
+				if (inputName === undefined) {
+					var input = element.parent()[0].querySelector('input, textarea, select');
+					inputName = input.getAttribute('name');
+				}
+
+				var form = controller;
+				var formName = controller.$name;
+
+				var markup = '<div class="help-block"' + 
+					'	ng-messages="' + formName + '.' + inputName + '.$error"' +
+					'	ng-show="' + formName + '.' + inputName + '.$dirty"' +
+					'	ng-messages-include="app/ui/forms/errorMessages.html">' +
+					'</div>';
+
+				element.append($compile(markup)(scope));
+			}
+		};
+	}
+
+}());
