9bbe4ac702a550a3d9c5018990cdf0eaee05697d
Jason Ody <jasonody@users.noreply.github.com>
2015-06-11 08:29:43 -0700

    Added http interceptor to include OAuth bearer token

---
+++ NamaBeer.Client/nama-framework/common/auth/tokenInterceptor.js
@@ -0,0 +1,34 @@
+﻿(function () {
+	'use strict';
+
+	angular
+		.module('nama.common')
+		.factory('tokenInterceptor', tokenInterceptor);
+	angular
+		.module('nama.common')
+		.config(function ($httpProvider) {
+
+			$httpProvider.interceptors.push('tokenInterceptor');
+		});
+
+	tokenInterceptor.$inject = ['$injector', '$q'];
+
+	function tokenInterceptor($injector, $q) {
+
+		return {
+			request: request
+		};
+
+		function request(config) {
+
+			var user = $injector.get('user');
+
+			if (user.profile.isLoggedIn) {
+				config.headers.Authorization = "Bearer " + user.profile.token;
+			}
+
+			return $q.when(config);
+		}
+	}
+
+}());
