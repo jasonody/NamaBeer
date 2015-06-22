be10ddafc441108dd689151251fdb13a8def1bf7
Jason Ody <jasonody@users.noreply.github.com>
2015-06-22 09:24:26 -0700

    First commit for autocomplete directive

---
+++ NamaBeer.Client/nama-framework/ui/autocomplete/autocompleteDirective.js
@@ -0,0 +1,45 @@
+﻿(function () {
+	'use strict';
+
+	angular
+		.module('nama.ui')
+		.directive('namaAutocomplete', autocomplete);
+
+	autocomplete.$inject = ['$http', '$timeout'];
+
+	function autocomplete($http, $timeout) {
+
+		return {
+			restrict: 'AE',
+			replace: true,
+			templateUrl: '/nama-framework/ui/autocomplete/autocomplete.html',
+			scope: {
+				model: '=',
+				placeholder: '@'
+			},
+			controllerAs: 'vm',
+			bindToController: true,
+			link: function (scope, element, attributes) {
+
+				$timeout(function () {
+
+					scope.vm.model = scope.vm.term;
+					$http.get(attributes.url + scope.vm.term)
+					.success(function (data) {
+
+						scope.vm.suggestions = data[attributes.resultsproperty];
+					});
+
+				}, 2000);
+			},
+			controller: function () {
+
+			}
+		};
+	}
+
+}());
+
+//http://www.htmlxprs.com/post/32/creating-an-angularjs-autocomplete-tag-input-widget
+//http://stackoverflow.com/questions/18460374/angularjs-autocomplete-from-http
+//https://github.com/JustGoscha/allmighty-autocomplete/blob/master/script/autocomplete.js
---
+++ NamaBeer.WebAPI/Controllers/StylesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.OData;
+
+namespace NamaBeer.WebAPI.Controllers
+{
+	[EnableCors("*", "*", "*")]
+    public class StylesController : ApiController
+    {
+		[EnableQuery(PageSize=5)]
+		public dynamic Get()
+		{
+			return new {
+				suggestions = new List<string> 
+				{
+					"India Pale Ale",
+					"Stout"
+				}.AsQueryable()
+			}; 
+		}
+    }
+}
