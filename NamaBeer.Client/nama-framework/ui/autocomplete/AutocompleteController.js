099a74e92d5c58cbaea949a065e3cc54d3ee817c
Jason Ody <jasonody@users.noreply.github.com>
2015-07-25 15:50:54 -0700

    Created external controller for autocomplete directive

---
+++ NamaBeer.Client/nama-framework/ui/autocomplete/AutocompleteController.js
@@ -0,0 +1,42 @@
+﻿(function () {
+	'use strict';
+
+	angular
+		.module('nama.ui')
+		.controller('AutocompleteController', AutocompleteController);
+
+	AutocompleteController.$inject = ['$timeout', '$http'];
+
+	function AutocompleteController($timeout, $http) {
+
+		var vm = this;
+		var timeout;
+
+		vm.terms = vm.model;
+		vm.showSuggestions = false;
+		vm.selectSuggestion = function (suggestion) {
+
+			vm.model = vm.terms = suggestion;
+			vm.showSuggestions = false;
+		};
+
+		vm.getSuggestions = function () {
+
+			$timeout.cancel(timeout);
+
+			if (vm.terms && vm.terms.length > 0) {
+				timeout = $timeout(function () {
+
+					$http.get(vm.url + vm.terms)
+					.success(function (data) {
+
+						if (data[vm.resultsProperty].length > 0) {
+							vm.suggestions = data[vm.resultsProperty];
+							vm.showSuggestions = true;
+						}
+					});
+				}, 1000);
+			}
+		}
+	}
+}());
--- NamaBeer.Client/nama-framework/ui/autocomplete/autocompleteDirective.js
+++ NamaBeer.Client/nama-framework/ui/autocomplete/autocompleteDirective.js
@@ -37,39 +37,10 @@
 						element.removeAttr(attribute);
 					}
 				}
+
+				$compile(input)(scope);
 			},
-			controller: function ($timeout) {
-
-				var vm = this;
-				var timeout;
-				
-				vm.terms = vm.model;
-				vm.showSuggestions = false;
-				vm.selectSuggestion = function (suggestion) {
-
-					vm.model = vm.terms = suggestion;
-					vm.showSuggestions = false;
-				};
-
-				vm.getSuggestions = function () {
-
-					$timeout.cancel(timeout);
-
-					if (vm.terms && vm.terms.length > 0) {
-						timeout = $timeout(function () {
-
-							$http.get(vm.url + vm.terms)
-							.success(function (data) {
-
-								if (data[vm.resultsProperty].length > 0) {
-									vm.suggestions = data[vm.resultsProperty];
-									vm.showSuggestions = true;
-								}
-							});
-						}, 1000);
-					}
-				}
-			}
+			controller: 'AutocompleteController'
 		};
 	}
 
