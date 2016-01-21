163b09874bb5bef541d57f781dd45a3657ae57b8
Jason Ody <jasonody@users.noreply.github.com>
2016-01-20 22:27:00 -0800

    Added Expandable Content directive to Nama Framework

---
+++ NamaBeer.Client/nama-framework/ui/expandableContent/expandableContentController.js
@@ -0,0 +1,20 @@
+﻿(function () {
+	'use stict';
+
+	angular
+		.module('nama.ui')
+		.controller('ExpandableContentController', ExpandableContentController);
+
+	ExpandableContentController.$inject = [];
+
+	function ExpandableContentController() {
+
+		var vm = this;
+		vm.expanded = false;
+
+		vm.toggle = function () {
+
+			vm.expanded = !vm.expanded;
+		};
+	}
+}());
---
+++ NamaBeer.Client/nama-framework/ui/expandableContent/expandableContentDirective.js
@@ -0,0 +1,21 @@
+﻿(function () {
+	'use stict';
+
+	angular
+		.module('nama.ui')
+		.directive('namaExpandableContent', expandableContent);
+
+	expandableContent.$inject = [];
+
+	function expandableContent() {
+
+		return {
+			restrict: 'AE',
+			templateUrl: '/nama-framework/ui/expandableContent/expandableContent.html',
+			transclude: true,
+			controller: 'ExpandableContentController',
+			controllerAs: 'vm',
+			scope: true
+		};
+	}
+}());
