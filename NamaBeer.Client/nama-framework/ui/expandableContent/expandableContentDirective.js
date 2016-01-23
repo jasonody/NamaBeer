25fa9ed40b66400bd7f0fb54d533693ee2a6c856
Jason Ody <jasonody@users.noreply.github.com>
2016-01-22 17:49:25 -0800

    Added animation to Expandable Content directive

--- NamaBeer.Client/nama-framework/ui/expandableContent/expandableContentDirective.js
+++ NamaBeer.Client/nama-framework/ui/expandableContent/expandableContentDirective.js
@@ -13,9 +13,18 @@
 			restrict: 'AE',
 			templateUrl: '/nama-framework/ui/expandableContent/expandableContent.html',
 			transclude: true,
+			replace: true,
+			link: link,
 			controller: 'ExpandableContentController',
 			controllerAs: 'vm',
 			scope: true
 		};
+	}
+
+	function link(scope, element, attrs) {
+
+		if (attrs.animate && attrs.animate === 'false') {
+			element.find('ng-transclude').parent().removeClass('animate-show');
+		}
 	}
 }());
